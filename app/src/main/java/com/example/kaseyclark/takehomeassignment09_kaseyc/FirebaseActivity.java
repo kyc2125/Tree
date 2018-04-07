756474ffa13197932cc967dcf4fdda2ba64d2e01
Kasey <kyc2125@tc.columbia.edu>
2018-04-07 11:48:28 -0400

    Added Date

--- app/src/main/java/com/example/kaseyclark/takehomeassignment09_kaseyc/FirebaseActivity.java
+++ app/src/main/java/com/example/kaseyclark/takehomeassignment09_kaseyc/FirebaseActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import java.util.Date;
 
 public class FirebaseActivity extends AppCompatActivity {
 
@@ -17,6 +19,9 @@ public class FirebaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
+        TextView textView =(TextView)findViewById(R.id.dateText);
+        Date date = new Date();
+        textView.setText(date.toString());
     }
     public void set (View view) {
         treeRef.setValue(new Tree("oak", true, 20));
